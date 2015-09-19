019c46809c0daf626f86c30a99b8a24369b872a6
pachamaltese <mauriciovargas@ug.uchile.cl>
2015-09-19 18:05:05 -0300

    19-sep

--- project/elder-population-in-chile/ui.R
+++ project/elder-population-in-chile/ui.R
@@ -6,17 +6,16 @@ demografia <- read.csv('instancia.csv')
 
 # Define UI 
 shinyUI(fluidPage(
+  tags$head(includeScript("google-analytics.js")),
+  
   # Title
   titlePanel("Elder Population (64+ years) Count in Chile (1992-2006)"),
   p("According to several studies in Chile we have an aging population and this means to change Public Policy guidelines. In 1995 we had 36 millions of elders in Latin America and by 2025 we will have 72+ millions of elders."),
-  p("Therefore, it would be interesting to visualize how our elder population is aging. In the pot we can observe that as years are passing, we have more and more 70+ years elders."),
   p("The plot shows the elder population (64+ years) classified by age and as females and males (I also show the total population count) from 1992 to 2006."), 
   p(" "),
   em("A good short article about aging population in Chile and Latin America is found in ", a("http://www.gerontologia.uchile.cl/docs/chien3.htm", href="http://www.gerontologia.uchile.cl/docs/chien3.htm")),
   p(" "),
-  em("The data was obtained from the Human Mortality Database:", a("http://www.mortality.org/cgi-bin/hmd/country.php?cntr=CHL&level=1", href="http://www.mortality.org/cgi-bin/hmd/country.php?cntr=CHL&level=1")),
-  p(" "),
-  
+  hr(),
   # Sidebar 
   sidebarLayout(
     sidebarPanel(
@@ -76,13 +75,14 @@ shinyUI(fluidPage(
                   max = 2006,
                   value = c(1992, 2006), 
                   format = "0000"), 
-      helpText(p("Move the scroll bar to adjust the period of time displayed."), p("Plot Created on 21 Feb 2015 by Pachamaltese"), img(src="https://raw.githubusercontent.com/pachamaltese/Developing-Data-Products/master/proyecto/logo.jpg"), p("This plot was created for JHU's Developing Data Products MOOC."))
+      helpText(p("Move the scroll bar to adjust the period of time displayed."), p("Plot Created on 21 Feb 2015 by Pachamaltese"), img(src="https://raw.githubusercontent.com/pachamaltese/Developing-Data-Products/master/project/elder-population-in-chile/logo.jpg"), p("This plot was created for JHU's Developing Data Products MOOC."))
     ),
     
     # Show the plot 
     mainPanel(
-    strong("Give the plot a little time to load (the dataset is not small). The chart can be downloaded as PNG, PDF or SVG using the upper right icon in the plot. You can also show/hide series by clicking them below the plot."),
-    showOutput("lexplot", "highcharts")
+    strong("Give the plot a little time to load (the dataset is not small). You can also show/hide series by clicking them below the plot."),
+    showOutput("lexplot", "highcharts"),
+    em("The data was obtained from the Human Mortality Database:", a("http://www.mortality.org/cgi-bin/hmd/country.php?cntr=CHL&level=1", href="http://www.mortality.org/cgi-bin/hmd/country.php?cntr=CHL&level=1"))
     )
   )
 ))
